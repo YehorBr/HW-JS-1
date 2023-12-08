56b007a28e8acd3a065d177c24422e2262866269
Yehor <yehor12344321@gmail.com>
2023-12-08 13:53:00 +0200

    зберігає зміни

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const age = 15;
+console.log(age);
+
+const name = "Yehor";
+console.log(name);
+
+const isStudent = false;
+console.log(isStudent);
+
+const  myString = "1234";
+console.log(myString);
+
+let myNumber = 13;
+myNumber += 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const yourName = prompt("Твоє ім'я?");
+console.log(yourName);
+alert("Привіт "+ yourName);
+
+confirm("Так чи ні");
+
+alert("дія небезпечна! Підтвердіть дію.");
+confirm("Підтвердити дію:");
+
+// console.log(userName);
+// let userName = "yusgdhsdgjhk";
+// console.log(userName);
+
+// userName = "afafaf";
+// console.log(userName);
+
+// const student = "student";
+
+// userName = student;
+// console.log(userName);
+
+
+
+
